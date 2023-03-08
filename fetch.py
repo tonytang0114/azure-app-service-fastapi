e57b71c3cc6168e319d0e932ad553e4fa9ff9aa2
roshmadosh <hiroshi.nobuoka@gmail.com>
2023-03-07 21:22:02 -0500

    simple lin reg model for predicting happiness score of a pokemon

---
+++ fetch.py
@@ -0,0 +1,27 @@
+import requests
+import json
+
+'''
+A module for fetching data from the Pokemon API and saving it to a JSON file 
+called "poke_data.json".
+
+To generate the JSON file, run `python3 fetch.py` from this project's root directory.
+'''
+
+poke_list = []
+for ind in range(1, 152):
+    link = 'https://pokeapi.co/api/v2/pokemon-species/{}/'.format(ind)
+    print('Fetching from', link, '...')
+    fetched = requests.get(link)
+
+    data = fetched.text
+    results = json.loads(data)
+    poke_list.append({
+        'name': results['name'],
+        'color': results['color']['name'],
+        'shape': results['shape']['name'],
+        'base_happiness': results['base_happiness']
+    })
+
+with open('poke_data.json', 'w+') as f:
+    json.dump(poke_list, f)
--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 from fastapi import FastAPI
+from joblib import load
+import json
 
+# FastAPI is a framework for creating web APIs
 app = FastAPI()
 
+'''
+GET Request
+Path structure: http://localhost:8000/
+
+A standard controller to confirm you are able to connect to your API.
+'''
 @app.get("/")
 def index():
     return "This is a REST API for enabling my ML model to be used by other services."
 
-@app.get("/users")
-def getUsers():
-    return [
-        {
-            "name": "hiroshi",
-            "id": 1
-        },
-        {
-            "name": "else",
-            "id": 2
-        }
-    ]
+'''
+POST Request
+Path structure: http://localhost:8000/predict?color={enter_color}&shape={enter_shape}
+
+A controller for predicting a base happiness score for a pokemon based on their color and shape.
+
+Valid values for query parameters:
+color: ["black", "blue", "brown", "gray", "green", "pink", "purple", "red", "white", "yellow"]
+
+shape: ["armor", "arms", "ball", "blob", "bug-wings", "fish", "head", "humanoid", "legs", 
+"quadruped", "squiggle", "tentacles", "upright", "wings"]
+'''
+@app.post("/predict")
+def predict_happiness(color = 'black', shape = 'armor'):
+
+    # Extract dictionaries saved from our jupyter notebook that map a category code to its name.
+    with open('./mappers/color_mapper.json', 'r') as colorfile:
+        color_mapper = json.load(colorfile)
+    with open('./mappers/shape_mapper.json', 'r') as shapefile:
+        shape_mapper = json.load(shapefile)
+
+    # Map the query parameters "color" and "shape" to its corresponding category codes
+    color_code = [int(k) for k,v in color_mapper.items() if v == color][0]
+    shape_code = [int(k) for k, v in shape_mapper.items() if v == shape][0]
+
+    # Load the model we saved from our jupyter notebook and predict a happiness score.
+    model = load('my_model.joblib')
+    score = model.predict([[color_code, shape_code]])
+
+    return { "happiness_score": score[0][0] }
